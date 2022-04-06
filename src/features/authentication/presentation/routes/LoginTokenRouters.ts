303fda20779422759ad064dcb0eb7ea8176f56db
JAMERSON FELIPE PEREIRA PAZ <86246952+Ljames666@users.noreply.github.com>
2022-04-06 16:29:58 -0300

    add userRepository

--- src/features/authentication/presentation/routes/LoginTokenRouters.ts
+++ src/features/authentication/presentation/routes/LoginTokenRouters.ts
@@ -1,15 +1,15 @@
-import { Request, Response, Router } from "express";
-import { requestProprietary } from "../../../../core/infra/Middlewares/middlewares";
-import { RouterContract } from "../../../../core/presentation/contracts/routeContract";
-import { validateLogin } from "../../infra/middlewares/validateLogin";
-import { UserRepository } from "../../../users/infra/repository/UserRepository";
-import { TokenRepository } from "../../infra/repository/TokenRepository";
-import { GetTokenController } from "../../presentation/controllers/GetTokenController";
-import { LoginController } from "../../presentation/controllers/LoginController";
-import { LogoffController } from "../../presentation/controllers/logoffController";
-import { LoginService } from "../../domain/services/LoginService";
-import { GetTokenService } from "../../domain/services/GetTokenService";
-import { LogoffService } from "../../domain/services/LogoffService";
+import { Request, Response, Router } from 'express';
+import { requestProprietary } from '../../../../core/infra/Middlewares/middlewares';
+import { RouterContract } from '../../../../core/presentation/contracts/routeContract';
+import { validateLogin } from '../../infra/middlewares/validateLogin';
+import { UserRepository } from '../../../users/infra/repository/UserRepository';
+import { TokenRepository } from '../../infra/repository/TokenRepository';
+import { GetTokenController } from '../../presentation/controllers/GetTokenController';
+import { LoginController } from '../../presentation/controllers/LoginController';
+import { LogoffController } from '../../presentation/controllers/logoffController';
+import { LoginService } from '../../domain/services/LoginService';
+import { GetTokenService } from '../../domain/services/GetTokenService';
+import { LogoffService } from '../../domain/services/LogoffService';
 
 export class AuthRoutes implements RouterContract {
   static getRoutes() {
@@ -19,7 +19,7 @@ export class AuthRoutes implements RouterContract {
     const loginService = new LoginService(tokenRepository, userRepository);
     const loginController = new LoginController(loginService);
 
-    const getTokenService = new GetTokenService(tokenRepository);
+    const getTokenService = new GetTokenService(tokenRepository, userRepository);
     const getTokenController = new GetTokenController(getTokenService);
 
     const logoffService = new LogoffService(tokenRepository);
@@ -28,15 +28,15 @@ export class AuthRoutes implements RouterContract {
     const router = Router();
     router.use(requestProprietary);
 
-    router.post("/", validateLogin, (req: Request, res: Response) => {
+    router.post('/', validateLogin, (req: Request, res: Response) => {
       loginController.handle(req, res);
     });
 
-    router.get("/:id", (req: Request, res: Response) => {
+    router.get('/:id', (req: Request, res: Response) => {
       getTokenController.handle(req, res);
     });
 
-    router.delete("/:id", (req: Request, res: Response) => {
+    router.delete('/:id', (req: Request, res: Response) => {
       logoffController.handle(req, res);
     });
     return router;
