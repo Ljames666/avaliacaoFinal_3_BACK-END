ecbdd76a17c9381895f52270a52c88cce612be34
JAMERSON FELIPE PEREIRA PAZ <86246952+Ljames666@users.noreply.github.com>
2022-04-06 16:18:28 -0300

    configura reposta

--- src/features/authentication/domain/services/GetTokenService.ts
+++ src/features/authentication/domain/services/GetTokenService.ts
@@ -1,17 +1,33 @@
 import { IService } from '../../../../core/domain/interface/IService';
+import { UserRepository } from '../../../users/infra/repository/UserRepository';
 import { TokenRepository } from '../../infra/repository/TokenRepository';
 
 export class GetTokenService implements IService {
-  constructor(private repository: TokenRepository) {}
+  constructor(private repository: TokenRepository, private userRepository: UserRepository) {}
   async execute(id: string) {
     try {
       const activeToken = await this.repository.findOneToken(id);
-
+      const user = await this.userRepository.findOne(activeToken.user_id);
       if (!activeToken) {
         throw new Error(`Authorization is denied for ${id}`);
       }
-
-      return activeToken;
+      const userToToken = {
+        role: 'user',
+        access_token: activeToken.id,
+        data: {
+          displayName: user.name,
+          photoURL: `https://avatars.dicebear.com/api/croodles-neutral/${user.username}.svg`,
+          email: user.email,
+          shortcuts: ['calendar', 'mail', 'contacts', 'todo'],
+        },
+        user: {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+        meta: user,
+      };
+      return userToToken;
     } catch (error) {
       throw new Error(`${error}`);
     }
